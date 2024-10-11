c5c0b9631229d93c32fa5fcf6c5f207761082f5f
czeria <longtlhps38519@gmail.com>
2024-10-11 14:19:45 +0700

    siuuu

--- scroll.js
+++ scroll.js
@@ -2,7 +2,7 @@ window.onscroll = function() {
     var header = document.getElementById("main-header");
 
     if (window.scrollY > 50) {
-        header.style.backgroundColor = "rgba(128, 0, 128, 0.7)"; // Purple with 70% opacity
+        header.style.backgroundColor = "rgba(68, 0, 128, 0.8)"; // Purple with 70% opacity
     } else {
         header.style.backgroundColor = "transparent"; // Fully transparent
     }
